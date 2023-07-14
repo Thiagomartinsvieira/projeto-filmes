fc357ba02e9f1fe2298dcc630693b5dd3156cbdc
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-14 16:07:03 -0300

    Loading movies

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState} from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 // URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
 
 function Home(){
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
 
   useEffect(()=>{
@@ -18,7 +21,9 @@ function Home(){
         }
       })
 
-      console.log(response.data.results);
+      //console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10))
+      setLoading(false);
 
     }
 
@@ -26,9 +31,29 @@ function Home(){
 
   }, [])
 
+
+
+  if(loading){
+    return(
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    )
+  }
+
   return(
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
     </div>
   )
 }
